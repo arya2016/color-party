4d9477af06e389b2582e414c43b4508ada70e206
galaxycoder <skmonirtys@gmail.com>
2025-03-10 17:23:50 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function makered(){
+    document.body.style.backgroundColor = 'red';
+ document.querySelector('h1').innerHTML = 'i'
+}
+function makeorange(){
+    document.body.style.backgroundColor = 'orange';
+     document.querySelector('h1').innerHTML = 'cant'
+}
+function makeyellow(){
+    document.body.style.backgroundColor = 'yellow';
+     document.querySelector('h1').innerHTML = 'bellive'
+}
+function makegreen(){
+    document.body.style.backgroundColor = 'green';
+ document.querySelector('h1').innerHTML = 'weregoing to '
+}
+function makeblue(){
+    document.body.style.backgroundColor = 'blue'
+     document.querySelector('h1').innerHTML = 'go with shophing'
+}
+function makeindigo(){
+    document.body.style.backgroundColor = 'indigo'
+   document.querySelector('h1').innerHTML = 'waitfor it....'
+}
+function makepurple(){
+    document.body.style.backgroundColor = 'purple'
+    document.querySelector('h1').innerHTML = 'giyu'
+}
